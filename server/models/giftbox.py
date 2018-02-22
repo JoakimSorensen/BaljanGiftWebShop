044394d8c7c82abe07a5226a189ad88f8d763fec
Felicia Dahlström <felda975@student.liu.se>
2018-02-22 10:22:30 +0100

    New branch Felicia, added model giftbox

---
+++ server/models/giftbox.py
@@ -0,0 +1,16 @@
+from server import db
+from server.models.shared_model import SharedModel
+
+
+class Giftbox(SharedModel):
+    id = db.Column(db.Text, index=True, unique=True)
+    name = db.Column(db.Text, index=True)
+    price = db.Column(db.Integer, index=True)
+    description = db.Column(db.Text, index=True)
+    image = db.Column(db.Text)
+    #Hur refererar vi till en bild? en .svg-fil?
+
+    mutable_fields = {name, price, description, image}
+    required_fields = {id, name, price}
+    excluded_fields = set()
+

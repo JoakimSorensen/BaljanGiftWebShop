df3efa973bfa2c61a209691deb1fbc9d1a0aaaa0
filleokus <filleokus@gmail.com>
2018-02-28 00:44:35 +0100

    Refactor creation of mock data

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import mockdata
-import data
 from server import app
 
 
@@ -13,9 +12,6 @@ def reset_db():
 
     # Create mock data in the database
     mockdata.create_mock_data()
-
-    # Create data in the database
-    data.create_data()
 
 
 if __name__ == '__main__':

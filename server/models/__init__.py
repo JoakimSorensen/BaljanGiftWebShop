740a7cbaff79249474f90d1dc9e2b3c53e176c5f
filleokus <filleokus@gmail.com>
2018-02-28 00:42:17 +0100

    Remove premature models

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,7 +1,3 @@
 from server.models.user import *
 from server.models.product import *
-from server.models.box_content import *
-from server.models.credit_card import *
 from server.models.gift_box import *
-from server.models.payment import *
-

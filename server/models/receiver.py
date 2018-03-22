1e663b0df5265381bf7383978562c69c71211b65
filleokus <filleokus@gmail.com>
2018-03-22 14:30:17 +0100

    Create convenience method to create `Receiver`

--- server/models/receiver.py
+++ server/models/receiver.py
@@ -1,5 +1,6 @@
 from server import db
 from server.models.shared_model import SharedModel
+from utils import format_phone_number
 
 
 class Receiver(SharedModel):
@@ -10,6 +11,11 @@ class Receiver(SharedModel):
     mutable_fields = set()
     required_fields = {liu_id, name, phone}
     excluded_fields = set()
+
+    @classmethod
+    def create_receiver(cls, name, phone, liu_id):
+        formatted_phone_number = format_phone_number(phone)
+        return Receiver.add(name=name, phone=formatted_phone_number, liu_id=liu_id)
 
     def __repr__(self):
         return self.name

cbf628a68600c55534123dc18d7ae8052e83ba6e
filleokus <filleokus@gmail.com>
2018-03-22 15:13:07 +0100

    Translate confirmation email to swedish

--- server/notifications/templates.py
+++ server/notifications/templates.py
@@ -3,23 +3,27 @@ def order_confirmation_email(order):
     receiver = order.receiver
     giftbox = order.giftbox
 
-    subject = "Baljangavan: Order confirmation, {}".format(order.date)
+    subject = "Baljangavan: Orderbekräftelse {}".format(order.date)
 
-    message = """ We have received your order! 
-        Your name: {name}
-        Receiver's name: {receiver_name}
-        Receiver's LiU ID: {receiver_liu_id}
-        Receiver's phone: {receiver_phone}
-        Price: {price}
-        Gift: {gift}
-        Message: {message}
-        Status: {status} 
-        You can use the token below to see current status
-        Token: {token}
-        """.format(name=buyer.name, receiver_name=receiver.name,
-                   receiver_liu_id=receiver.liu_id, receiver_phone=receiver.phone,
-                   price=order.price, gift=giftbox.name, message=order.message,
-                   status=order.status, token=order.token)
+    message = """
+    Vi har mottagit din beställning! 
+    
+    Ditt namn: {name}
+    Mottagarens namn: {receiver_name}
+    Mottagarens LiU ID: {receiver_liu_id}
+    Mottagarens telefonnummer: {receiver_phone}
+        
+    Pris: {price}
+    Gåva: {gift}
+    Meddelande: {message}
+                
+    Bekräftelsenummret {token} kommer användas av gåvans mottagare för att hämta ut den i Baljan. 
+        
+    För att se status på beställningen, gå in på http://localhost:5000/order?token={token}
+    """.format(name=buyer.name, receiver_name=receiver.name,
+               receiver_liu_id=receiver.liu_id, receiver_phone=receiver.phone,
+               price=order.price, gift=giftbox.name, message=order.message,
+               token=order.token)
 
     return subject, message
 

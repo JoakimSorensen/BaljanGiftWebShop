0efba30138427f34643c1976eedc75a6e8d181b8
Felicia Dahlström <felda975@student.liu.se>
2018-04-19 14:10:25 +0200

    Added informational boxes at click

--- server/static/js/contact.js
+++ server/static/js/contact.js
@@ -1,25 +1,17 @@
 $(document).ready(function() {
     $(".explanation-wrapper").css({'visibility':'hidden'});
-    $(".guide-img").on("click", function() {
+    $(".contact-img").on("click", function() {
         var id = this.id;
-        if (!$("#" + id + "-guide").hasClass("active-text")) {
-            $("#" + id + "-guide").css({'visibility':'visible'});
-            $("#" + id + "-guide").addClass("active-text");
+        if (!$("#" + id + "-info").hasClass("active-text")) {
+            $("#" + id + "-info").css({'visibility':'visible'});
+            $("#" + id + "-info").addClass("active-text");
 
         } else {
-            $("#" + id + "-guide").css({'visibility':'hidden'});
-            $("#" + id + "-guide").removeClass("active-text");
+            $("#" + id + "-info").css({'visibility':'hidden'});
+            $("#" + id + "-info").removeClass("active-text");
         };
     });
 
-});
-
-$(window).on('resize', function() {
-    if ($(window).width() < 1000) {
-        $(".arrow-img").hide();
-    } else {
-        $(".arrow-img").show();
-    };
 });
 
 $('.get-started').on('click', function(event) {
